21ff34cf000662fb2cc40e3e6f9e29a4e1f2c2ff
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-10-11 14:13:42 +0545

    feat: introduction page ui impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nock/presentation/signup/view/get_started.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Nock',
-      home: Scaffold(
-        body: Center(
-          child: Text('Nock'),
-        ),
-      ),
+      home: GetStarted()
     );
   }
 }
---
+++ lib/presentation/signup/view/get_started.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:nock/Components/colors.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.125,
+            ),
+            Image.asset("assets/introduction.png"),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16, 30, 0, 0),
+              child: Row(
+                children: [
+                  Text(
+                    "Welcome to ",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: "Product Sans",
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black),
+                  ),
+                  Text(
+                    "Nock",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: "Product Sans",
+                        fontWeight: FontWeight.normal,
+                        color: blueColor),
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16, 10, 0, 0),
+              child: Text(
+                "Smart Attendance Tracking App",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: "Product Sans",
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 25, 8, 0),
+              child: Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: 60,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: blueColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Get Started",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: "Product Sans",
+                              fontWeight: FontWeight.normal,
+                              color: Colors.white),
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        Image.asset(
+                          "assets/arrow.png",
+                          height: 20,
+                          width: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 16.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        "Made with ❤️ By ",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontFamily: "Product Sans",
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text(
+                            "G",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontFamily: "Product Sans",
+                                fontWeight: FontWeight.normal,
+                                color: gdscBlue),
+                          ),
+                          Text(
+                            "D",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontFamily: "Product Sans",
+                                fontWeight: FontWeight.normal,
+                                color: gdscYellow),
+                          ),
+                          Text(
+                            "S",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontFamily: "Product Sans",
+                                fontWeight: FontWeight.normal,
+                                color: gdscRed),
+                          ),
+                          Text(
+                            "C",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontFamily: "Product Sans",
+                                fontWeight: FontWeight.normal,
+                                color: gdscGreen),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
